4f119ca750ebceccff8f93dfd89ba6843505d094
theoleuthardt <theoleuthardt12@gmail.com>
2025-02-08 15:44:09 +0100

    fix: favicon and icon on every page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Werkzeugkiste",
   description: "Collection of useful digital utilities",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+    },
+  ],
 };
 
 export default function RootLayout({
